e88ddd34b709dbaeccd467d78581b51880736da1
Alessandro Fugazza <alessandro.fugazza01@gmail.com>
2023-07-13 20:15:47 +0200

    various bug fixes and improvements

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
-const tombolaCellNum = 76;
-const playerCellNum = 24;
+const tombolaCellCount = 76;
+const playerCellCount = 24;
 const tombolaSheet = document.getElementById("tombolaSheet");
 const playerSheet = document.getElementById("playerSheet");
 const customPlayerSheet = document.getElementById("customPlayerSheet");
 
+const numbersToDrow = [];
+let remainingNumbers = tombolaCellCount;
+
 const createTombolaSheet = () => {
-  for (let i = 0; i < tombolaCellNum; i++) {
+  for (let i = 0; i < tombolaCellCount; i++) {
+    numbersToDrow.push(i + 1);
     const newCell = document.createElement("div");
     newCell.classList.add("cell");
     newCell.classList.add(`cell${i}`);
@@ -17,52 +21,69 @@ const createTombolaSheet = () => {
   }
 };
 
-let drawnNumbers = [];
-
-const randCell = () => {
-  const randNum = Math.floor(Math.random() * tombolaCellNum);
-  drawnNumbers.forEach(number => {
-    if (number === randNum) {
-      randCell();
-    }
-  });
-  drawnNumbers.push(randNum);
-  return randNum;
-};
-
 const draw = () => {
-  const randNum = randCell();
-  const cells = document.getElementsByClassName(`cell${randNum}`);
+  const numberIndexToRemove = Math.floor(Math.random() * remainingNumbers);
+  console.log(numberIndexToRemove);
+  let numberDrawn = numbersToDrow.splice(numberIndexToRemove, 1);
+  console.log(numbersToDrow);
+  numberDrawn = numberDrawn[0];
+  const cells = document.getElementsByClassName(`cell${numberDrawn - 1}`);
   for (let i = 0; i < cells.length; i++) {
     cells[i].style.backgroundColor = "pink";
+  }
+  remainingNumbers--;
+  if (remainingNumbers === 0) {
+    setTimeout(() => alert("game over"), 0);
+    return;
   }
 };
 
 const createPlayerSheet = () => {
-  for (let i = 0; i < playerCellNum; i++) {
-    const cellNum = randCell();
+  for (let i = 0; i < playerCellCount; i++) {
+    const numberIndexToRemove = Math.floor(Math.random() * remainingNumbers);
+    remainingNumbers--;
+    numberDrawn = numbersToDrow.splice(numberIndexToRemove, 1);
+    numberDrawn = numberDrawn[0];
     const newCell = document.createElement("div");
     newCell.classList.add("cell");
-    newCell.classList.add(`cell${cellNum}`);
+    newCell.classList.add(`cell${numberDrawn - 1}`);
     const number = document.createElement("p");
-    number.innerText = cellNum + 1;
+    number.innerText = numberDrawn;
     newCell.appendChild(number);
     playerSheet.appendChild(newCell);
   }
+  numbersToDrow.length = 0;
+  for (let i = 0; i < tombolaCellCount; i++) {
+    numbersToDrow.push(i + 1);
+  }
+  remainingNumbers = tombolaCellCount;
 };
 
-const addToCustomSheet = e => {
-  const newCell = document.createElement("div");
-  const cellNum = e.target.innerText;
-  newCell.classList.add("cell");
-  newCell.classList.add(`cell${cellNum - 1}`);
-  const number = document.createElement("p");
-  number.innerText = cellNum;
-  newCell.appendChild(number);
+let chosenNumbersCount = 1;
 
+const addToCustomSheet = e => {
+  if (chosenNumbersCount > playerCellCount) {
+    alert("your sheet is full");
+    return;
+  }
+  const isChosen = e.currentTarget.classList.contains("chosen");
+  if (isChosen) {
+    alert("cannot choose the same number twice!");
+    return;
+  }
+  e.currentTarget.classList.add("chosen");
+  const number = e.target.innerText;
+  const newCell = document.createElement("div");
+
+  newCell.classList.add("cell");
+  newCell.classList.add(`cell${number - 1}`);
+  const cellContent = document.createElement("p");
+  cellContent.innerText = number;
+  newCell.appendChild(cellContent);
   customPlayerSheet.appendChild(newCell);
+  chosenNumbersCount++;
 };
 
 createTombolaSheet();
 createPlayerSheet();
-drawnNumbers = [];
+console.log(numbersToDrow);
